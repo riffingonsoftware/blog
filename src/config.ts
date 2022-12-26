2c0c6fd7af895f159145a1df519c4ec10ce7e70d
Ricardo Torres <riffingonsoftware@gmail.com>
2022-12-26 15:30:45 -0700

    Add Tailwind, start personalizing.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Riffing on Software';
+export const SITE_DESCRIPTION = "Strong opinions, loosely held because I'm probably wrong.";
