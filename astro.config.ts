ddcb43dd74117c6222f1efaba58892dec7125160
Ricardo Torres <riffingonsoftware@gmail.com>
2023-01-27 17:29:39 -0700

    Add comments.

--- astro.config.ts
+++ astro.config.ts
@@ -5,15 +5,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		extendDefaultPlugins: true,
-		remarkPlugins: [
-			codeTitle,
-		],
-	},
-	// compress should be last
-	integrations: [mdx(), sitemap(), tailwind(), compress(),],
-	site: 'https://riffingonsoftware.com',
+  markdown: {
+    remarkPlugins: [codeTitle]
+  },
+  // compress should be last
+  integrations: [mdx(), sitemap(), solidJs(), tailwind(), compress()],
+  site: 'https://riffingonsoftware.com'
 });
---
+++ src/components/Comments.tsx
@@ -0,0 +1,21 @@
+import { onMount } from "solid-js";
+
+onMount(() => {
+  const script = document.createElement("script");
+  const tag = document.getElementById('giscus');
+  script.src = "https://giscus.app/client.js";
+  script.setAttribute("data-repo", "riffingonsoftware/blog");
+  script.setAttribute("data-repo-id", "R_kgDOIrK8MQ");
+  script.setAttribute("data-category", "Giscus");
+  script.setAttribute("data-category-id", "DIC_kwDOIrK8Mc4CT2Ig");
+  script.setAttribute("data-mapping", "pathname");
+  script.setAttribute("data-strict", "1");
+  script.setAttribute("data-reactions-enabled", "1");
+  script.setAttribute("data-emit-metadata", "0");
+  script.setAttribute("data-input-position", "top");
+  script.setAttribute("data-theme", "preferred_color_scheme");
+  script.setAttribute("data-lang", "en");
+  script.setAttribute("data-loading", "lazy");
+  script.setAttribute("crossorigin", "anonymous");
+  script.setAttribute("async", "true");
+});
