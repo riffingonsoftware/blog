20db3d7ad26763640d7cdbb1c91995590bcbfa01
Ricardo Torres <riffingonsoftware@gmail.com>
2023-07-13 15:30:53 -0600

    pnpm up, tweaked some widths, fixed theming, oops.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			typography: {
-				DEFAULT: {
-					css: {
-						code: {
-							fontFamily: "Fira Code, monospace",
-							// span: {
-							//   fontFamily: "Fira Code, monospace",
-							// },
-						},
-					},
-				},
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            code: {
+              fontFamily: "Fira Code, monospace",
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
+};
