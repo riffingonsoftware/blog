1f8df1160858e27e3a163bf9ba496650073d0057
Ricardo Torres <riffingonsoftware@gmail.com>
2024-02-10 15:05:23 -0700

    pnpm up. Add OgImage back in. Try to fix comment loading.

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import codeTitle from "remark-code-title";
 import compress from "astro-compress";
 import mdx from "@astrojs/mdx";
@@ -6,15 +6,13 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
-import ogImage from "./src/integrations/OgImage.ts";
+import ogImage from "./src/integrations/OgImage";
 
 // https://astro.build/config
 export default defineConfig({
     markdown: {
         remarkPlugins: [codeTitle],
     },
-    // ogImage doesn't work on Netlify. Don't feel like troubleshooting, just copied the output from a previous run into images. Could regenerate locally.
-    // compress should be last
-    integrations: [mdx(), sitemap(), solidJs(), tailwind(), /*ogImage(),*/ compress(),],
+    integrations: [mdx(), sitemap(), solidJs(), tailwind(), ogImage(), compress(),],
     site: "https://riffingonsoftware.com",
 });
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,27 +1,28 @@
-import { onMount } from "solid-js";
+import { createEffect } from "solid-js";
 
 export default function Comments() {
+  createEffect(() => {
+    const script = document.createElement("script");
+    const tag = document.getElementById('giscus');
+    script.src = "https://giscus.app/client.js";
+    script.setAttribute("data-repo", "riffingonsoftware/blog");
+    script.setAttribute("data-repo-id", "R_kgDOIrK8MQ");
+    script.setAttribute("data-category", "Giscus");
+    script.setAttribute("data-category-id", "DIC_kwDOIrK8Mc4CT2Ig");
+    script.setAttribute("data-mapping", "pathname");
+    script.setAttribute("data-strict", "1");
+    script.setAttribute("data-reactions-enabled", "1");
+    script.setAttribute("data-emit-metadata", "0");
+    script.setAttribute("data-input-position", "top");
+    script.setAttribute("data-theme", "preferred_color_scheme");
+    script.setAttribute("data-lang", "en");
+    script.setAttribute("data-loading", "lazy");
+    script.setAttribute("crossorigin", "anonymous");
+    script.setAttribute("async", "true");
+    tag?.appendChild(script);
+  });
+
   return (
     <div id="giscus" />
   );
 }
-onMount(() => {
-  const script = document.createElement("script");
-  const tag = document.getElementById('giscus');
-  script.src = "https://giscus.app/client.js";
-  script.setAttribute("data-repo", "riffingonsoftware/blog");
-  script.setAttribute("data-repo-id", "R_kgDOIrK8MQ");
-  script.setAttribute("data-category", "Giscus");
-  script.setAttribute("data-category-id", "DIC_kwDOIrK8Mc4CT2Ig");
-  script.setAttribute("data-mapping", "pathname");
-  script.setAttribute("data-strict", "1");
-  script.setAttribute("data-reactions-enabled", "1");
-  script.setAttribute("data-emit-metadata", "0");
-  script.setAttribute("data-input-position", "top");
-  script.setAttribute("data-theme", "preferred_color_scheme");
-  script.setAttribute("data-lang", "en");
-  script.setAttribute("data-loading", "lazy");
-  script.setAttribute("crossorigin", "anonymous");
-  script.setAttribute("async", "true");
-  tag?.appendChild(script);
-});
