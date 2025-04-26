37ff666673498743ff1da85ef6f54f6da7ee56d7
Ricardo Torres <riffingonsoftware@gmail.com>
2025-04-26 11:43:37 -0600

    Clean slate.

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Riffing on Software";
-export const SITE_DESCRIPTION =
-  "Over analyzing everything, including these words.";
