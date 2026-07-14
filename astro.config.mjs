d4d3252e47b8d70dd5dff4e4bd9aa45a6ccc4af7
Ricardo Torres <riffingonsoftware@gmail.com>
2026-07-14 15:37:47 -0600

    Remove legacy remark pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
-import codeTitle from "remark-code-title";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://riffingonsoftware.com",
   image: {
     dangerouslyProcessSVG: true,
-  },
-  markdown: {
-    remarkPlugins: [codeTitle],
   },
   integrations: [
     sitemap(),
